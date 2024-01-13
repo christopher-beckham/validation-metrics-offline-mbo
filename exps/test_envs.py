1c130bf4a55f6728a7fc3f38ef35554307279a30
Christopher Beckham <christopher.j.beckham@gmail.com>
2024-01-13 14:38:15 -0500

    add exps folder

---
+++ exps/test_envs.py
@@ -0,0 +1,8 @@
+import design_bench
+
+ant_task = design_bench.make("AntMorphology-Exact-v0")
+kitty_task = design_bench.make("DKittyMorphology-Exact-v0")
+
+print("Ant:", ant_task)
+print("Kitty:", kitty_task)
+
